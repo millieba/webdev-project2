49fc31e66ea995c88a4a9dc95124c347b61d8dbd
Dominika Iza Kowalska <dominiik@stud.ntnu.no>
2022-09-20 13:33:41 +0200

    Added textbox and button

--- prosjekt-2/src/SendLink.tsx
+++ prosjekt-2/src/SendLink.tsx
@@ -3,7 +3,13 @@ import './App.css';
 function SendLink() {
   return (
     <div>
-
+      <form>
+        <label>
+          Link: 
+          <input type="text" name="link"/>
+        </label>
+        <input type="submit" value="submit"/>
+      </form>
     </div>
   );
 }
