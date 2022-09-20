ffdebb3c790f26fe451c3335eba998f6adf996ac
Karen Villmones <karenvi@stud.ntnu.no>
2022-09-20 16:11:09 +0200

    Added textfield for access token

--- prosjekt-2/src/components/SendLink.tsx
+++ prosjekt-2/src/components/SendLink.tsx
@@ -4,12 +4,17 @@ import { Button } from '@mui/material';
 
 
 function SendLink() {
+
+  
   return (
     // To adjust the colors on the MUI-components, use theme in index.tsx
       <div>
-        <Box sx={{ mt: "15px" }}>
-          <TextField id="send-link" variant='outlined' label="GitLab repo" size="small" />
-          <Button variant="contained">
+        <Box>
+          <TextField id="send-link-field" variant='outlined' label="GitLab repo" size="small" sx={{ mt: "15px" }} />
+          <br />
+          <TextField id="access-token-field" variant='outlined' label="Access token" size="small" sx={{ mt: "15px" }} />
+          <br />
+          <Button id="access-token" variant="contained" sx={{ mt: "15px" }}>
             Submit
           </Button>
         </Box>
