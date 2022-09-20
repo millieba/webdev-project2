c358f63d9c69118847ea6ff2de91a210ce5b9e67
Karen Villmones <karenvi@stud.ntnu.no>
2022-09-20 14:42:29 +0200

    implemented MUI, and changed field and button on SendLink-page

--- prosjekt-2/src/components/SendLink.tsx
+++ prosjekt-2/src/components/SendLink.tsx
@@ -1,15 +1,19 @@
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import { Button } from '@mui/material';
+
 
 function SendLink() {
   return (
-    <div>
-      <form>
-        <label>
-          Link: 
-          <input type="text" name="link"/>
-        </label>
-        <input type="submit" value="submit"/>
-      </form>
-    </div>
+    // To adjust the colors on the MUI-components, use theme in index.tsx
+      <div>
+        <Box sx={{ mt: "15px" }}>
+          <TextField id="send-link" variant='outlined' label="GitLab repo" size="small" />
+          <Button variant="contained">
+            Submit
+          </Button>
+        </Box>
+      </div>
   );
 }
 
