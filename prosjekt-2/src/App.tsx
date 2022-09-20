83d6f3cd12749586456a789ae21eabc4436776e9
millieba <millieba@hotmail.com>
2022-09-20 19:59:52 +0200

    Response from api request now displayed

--- prosjekt-2/src/App.tsx
+++ prosjekt-2/src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Connect from './api/api';
 import './App.css';
 import SendLink from './components/SendLink';
 
@@ -7,6 +8,7 @@ function App() {
     <div className="App">
       
   <SendLink />
+  <Connect />
     </div>
   );
 }
