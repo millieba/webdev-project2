7d9cf8b34371924ac3bdbfa1494c4436e899dd22
Karen Villmones <karenvi@stud.ntnu.no>
2022-09-22 21:59:00 +0200

    Added a modal to inform user about project ids

--- prosjekt-2/src/components/SendLink.tsx
+++ prosjekt-2/src/components/SendLink.tsx
@@ -1,12 +1,29 @@
-import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
-import { Button } from '@mui/material';
+import { Button, IconButton, InputAdornment, Box } from '@mui/material';
 import { useEffect, useState } from 'react';
 import Connect from '../api/api';
+import HelpOutlineIcon from '@mui/icons-material/HelpOutline';
+import Modal from '@mui/material/Modal';
+import ProjectIdImage from '../images/project-id.png';
+
+const styleModal = {
+  top: '30%',
+  left: '50%',
+  width: '40%',
+  position: 'absolute',
+  transform: 'translate(-50%, -50%)',
+  backgroundColor: '#ffffff',
+  p: 4,
+  overflow: 'auto',
+};
 
 function SendLink() {
 
   const [disableButton, setDisableButton] = useState(true);
+  const [open, setOpen] = useState(false);
+
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
 
   // To avoid sending new variables all the time to the API useeffect dependencies
   const [tempAccessToken, setTempAccessToken] = useState<string>();
@@ -37,7 +54,17 @@ function SendLink() {
             onChange={(e) => {
               setTempProjectId(e.target.value);
             }}
-            sx={{ mt: "15px" }} />
+            sx={{ mt: "15px", width: "250px" }}
+            InputProps={{
+              endAdornment: (
+                <InputAdornment position='end'>
+                  <IconButton
+                    onClick={handleOpen}>
+                    <HelpOutlineIcon />
+                  </IconButton>
+                </InputAdornment>
+              ),
+            }}/>
           <br />
           <TextField 
             id="access-token-field" 
@@ -47,7 +74,7 @@ function SendLink() {
             onChange={(e) => {
               setTempAccessToken(e.target.value);
             }}
-            sx={{ mt: "15px" }} />
+            sx={{ mt: "15px", width: "250px" }} />
           <br />
           <Button 
             id="access-token" 
@@ -64,6 +91,19 @@ function SendLink() {
         {accessToken !== undefined && projectId !== undefined ?
         <Connect accessToken={accessToken} projectId={projectId}/> :
         <></> }
+
+        <Modal
+          open={open}
+          onClose={handleClose}>
+            <Box sx={styleModal}>
+              <h2>What is Gitlab repository project-id?</h2>
+              <p>Your project's id can be found by going to the homepage of your Gitlab repository. In the picture below an example is highlighted with yellow.</p>
+              <img src={ProjectIdImage}/>
+            </Box>
+
+        </Modal>
+
+
       </div>
   );
 }
