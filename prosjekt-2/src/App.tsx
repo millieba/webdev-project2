9a29e02fd7d7100e6cceb08a4e416679a890f0cf
millieba <millieba@hotmail.com>
2022-09-22 21:26:43 +0200

    Switched to axios, validated input, error handling

--- prosjekt-2/src/App.tsx
+++ prosjekt-2/src/App.tsx
@@ -1,10 +1,10 @@
 import './App.css';
-import SendLink from './components/SendLink';
+import UserPick from './components/UserPick';
 
 function App() {
   return (
     <div className="App">
-    <SendLink />
+    <UserPick />
     </div>
   );
 }
--- prosjekt-2/src/components/SendLink.tsx
+++ prosjekt-2/src/components/SendLink.tsx
@@ -2,9 +2,15 @@ import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import { Button } from '@mui/material';
 import { useEffect, useState } from 'react';
-import Connect from '../api/api';
+import Connect from '../api/Connect';
 
-function SendLink() {
+interface Props {
+  userPick: string;
+  displayValue: string;
+  header: string;
+}
+
+function SendLink({ userPick, displayValue, header }: Props) {
 
   const [disableButton, setDisableButton] = useState(true);
 
@@ -17,54 +23,64 @@ function SendLink() {
   const [projectId, setProjectId] = useState<string>();
 
   useEffect(() => {
-    if (tempAccessToken && tempProjectId) {
+    if (tempAccessToken && tempProjectId && validateInputFields(tempAccessToken,tempProjectId)) {
       setDisableButton(false);
     } else {
       setDisableButton(true);
     }
   }, [tempAccessToken, tempProjectId]);
 
+  function validateInputFields(accessToken: string, projectId: string) {
+    if (projectId == null || !projectId.match(/\d+/))
+      return false;
+    if (accessToken == null || !accessToken.match(/[\w-+~.=/]+/))
+      return false;
+
+    return true;
+  }
 
   return (
     // To adjust the colors on the MUI-components, use theme in index.tsx
-      <div>
-        <Box>
-          <TextField 
-            id="send-link-field" 
-            variant='outlined' 
-            label="GitLab repo project id" 
-            size="small"
-            onChange={(e) => {
-              setTempProjectId(e.target.value);
-            }}
-            sx={{ mt: "15px" }} />
-          <br />
-          <TextField 
-            id="access-token-field" 
-            variant='outlined' 
-            label="Access token" 
-            size="small" 
-            onChange={(e) => {
-              setTempAccessToken(e.target.value);
-            }}
-            sx={{ mt: "15px" }} />
-          <br />
-          <Button 
-            id="access-token" 
-            variant="contained"
-            onClick={() => {
-              setAccessToken(tempAccessToken);
-              setProjectId(tempProjectId);
-            }}
-            disabled={disableButton}
-            sx={{ mt: "15px" }}>
-            Submit
-          </Button>
-        </Box>
-        {accessToken !== undefined && projectId !== undefined ?
-        <Connect accessToken={accessToken} projectId={projectId}/> :
-        <></> }
-      </div>
+    <div>
+      <Box>
+        <TextField
+          id="send-link-field"
+          variant='outlined'
+          label="GitLab repo project id"
+          size="small"
+          onChange={(e) => {
+            setTempProjectId(e.target.value);
+          }}
+          sx={{ mt: "15px" }} />
+        <br />
+        <TextField
+          id="access-token-field"
+          variant='outlined'
+          label="Access token"
+          size="small"
+          onChange={(e) => {
+            setTempAccessToken(e.target.value);
+          }}
+          sx={{ mt: "15px" }} />
+        <br />
+        <Button
+          id="access-token"
+          variant="contained"
+          onClick={() => {
+            setAccessToken(tempAccessToken);
+            setProjectId(tempProjectId);
+          }}
+          disabled={disableButton}
+          sx={{ mt: "15px" }}>
+          Submit
+        </Button>
+      </Box>
+      {accessToken !== undefined && projectId !== undefined && validateInputFields(accessToken, projectId) ?
+        <Connect accessToken={accessToken} projectId={projectId} userPick={userPick} displayValue={displayValue} header={header} /> :
+        <></>}
+
+      {/* <p>{userPick}</p> */}
+    </div>
   );
 }
 
