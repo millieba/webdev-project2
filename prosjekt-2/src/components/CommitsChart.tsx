535ed1e0b00ce8991f0618d7cd37f9366bce165d
Dominika Iza Kowalska <dominiik@stud.ntnu.no>
2022-09-27 20:24:12 +0200

    Removed unnecesarry code

--- prosjekt-2/src/components/CommitsChart.tsx
+++ prosjekt-2/src/components/CommitsChart.tsx
@@ -1,32 +1,21 @@
-import {ResponsiveContainer, PieChart, Pie, Cell, Tooltip} from "recharts";
-import { useEffect, useState } from "react";
-import { createSecureContext } from "tls";
+import {PieChart, Pie, Cell, Tooltip} from "recharts";
 
 interface Props{
     cleanedResultls: Array<any>
 }
 
 function CommitChart({cleanedResultls}:Props){
-    cleanedResultls.forEach((result) => {
-        console.log(result.committer)
-    });
 
-    const nameOccurrences = cleanedResultls.reduce( (previous, current) => (previous[current.committer] = (previous[current.committer] || 0)+1, previous), {} );
-        console.log(nameOccurrences)
+    const nameOccurrences = cleanedResultls.reduce( (previous, current) => (
+        previous[current.committer] = (previous[current.committer] || 0)+1, previous), {} 
+    );
 
     let commit_data: Array<any> =[]
-    Object.keys(nameOccurrences).forEach((committer) => commit_data.push({name:committer, value: nameOccurrences[committer]}))
-    console.log(commit_data)
+    Object.keys(nameOccurrences).forEach((committer) => commit_data.push(
+        {name:committer, value: nameOccurrences[committer]})
+    );
 
     const colors = ["#6f4d76", "#795780", "#83618a", "#8d6b94", "#a17fa8", "#b593bc", "#c9a7d0"]
-
-    //dummy data
-    const dummy_data = [
-        {name: "Dominika", value:4},
-        {name: "Millie", value: 5},
-        {name: "Karen", value: 7},
-        {name: "Mariell", value: 4}
-    ]
   
     return (
         <div className="chart">
