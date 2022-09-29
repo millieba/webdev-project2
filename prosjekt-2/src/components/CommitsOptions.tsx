6cc496fad581122b18c6d6cde18764662e62c450
Karen Villmones <karenvi@stud.ntnu.no>
2022-09-29 13:47:15 +0200

    #28 - Displays values ordered by date...

--- prosjekt-2/src/components/CommitsOptions.tsx
+++ prosjekt-2/src/components/CommitsOptions.tsx
@@ -2,29 +2,21 @@ import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
 import Select, { SelectChangeEvent } from '@mui/material/Select';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Checkbox, Grid, ListItemText, OutlinedInput } from '@mui/material';
 
 interface Props {
     cleanedResults: Array<any>;
 }
 
-const ITEM_HEIGHT = 48;
-const ITEM_PADDING_TOP = 8;
-const MenuProps = {
-  PaperProps: {
-    style: {
-      maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
-      width: 250,
-    },
-  },
-};
 
 
 function CommitsOptions({ cleanedResults }: Props) {
 
     const [personName, setPersonName] = useState<string[]>([]);
-
+    let filteredResults: {committerName: string; dateOfCommit: string; commitMessage: string; }[] = [];
+    
+    // handleChange is taken from https://codesandbox.io/s/urnvxd?file=/demo.tsx:1221-1940
     const handleChange = (event: SelectChangeEvent<typeof personName>) => {
         const {
           target: { value },
@@ -48,48 +40,53 @@ function CommitsOptions({ cleanedResults }: Props) {
     console.log(names);
     console.log(personName);
 
+    personName.map((committerName, j) => {
+        cleanedResults.map((result, i) => {
+            if (result.committer === committerName) {
+                let committerName = result.committer;
+                let dateOfCommit = result.committedDate;
+                let commitMessage = result.commitMessage;
+                filteredResults.push({ committerName: committerName, dateOfCommit: dateOfCommit, commitMessage: commitMessage });
+            }
+        })
+    })
+
+    let filteredResultsByDate = filteredResults.sort((a, b) => Date.parse(b.dateOfCommit) - Date.parse(a.dateOfCommit));
+
+    console.log(filteredResultsByDate);
+
+
     return (
         <div>
             {/* Inspiration from https://codesandbox.io/s/urnvxd?file=/demo.tsx:1221-1940 */}
-        <FormControl sx={{ m: 1, width: 300 }}>
-            <InputLabel id="checkbox-dropdown">Select names</InputLabel>
-            <Select
-            labelId="checkbox-dropdown"
-            id="select-multiple-dropdown"
-            multiple
-            value={personName}
-            onChange={handleChange}
-            input={<OutlinedInput label="Select names" />}
-            renderValue={(selected) => selected.join(', ')}
-            MenuProps={MenuProps}
-            >
-            {names.map((name) => (
-                <MenuItem key={name} value={name}>
-                <Checkbox checked={personName.indexOf(name) > -1} />
-                <ListItemText primary={name} />
-                </MenuItem>
-            ))}
-            </Select>
-        </FormControl>
+                <FormControl sx={{ m: 1, width: 300 }}>
+                    <InputLabel id="checkbox-dropdown">Select names</InputLabel>
+                    <Select
+                    labelId="checkbox-dropdown"
+                    id="select-multiple-dropdown"
+                    multiple
+                    value={personName}
+                    onChange={handleChange}
+                    input={<OutlinedInput label="Select names" />}
+                    renderValue={(selected) => selected.join(', ')}
+                    >
+                    {names.map((name) => (
+                        <MenuItem key={name} value={name}>
+                        <Checkbox checked={personName.indexOf(name) > -1} />
+                        <ListItemText primary={name} />
+                        </MenuItem>
+                    ))}
+                    </Select>
+                </FormControl>
 
-        {personName.map((committerName, j) => (
-            <div key={j}>
-            {cleanedResults.map((result, i) => (
-            <div key={i}>
-                
-                {result.committer === committerName  ? 
-                <Grid container direction="column" justifyContent="flex-start" alignItems="center" sx={{ m: '5px', backgroundColor: '#9dbbae', borderRadius: "10px", p: "5px" }}>
-                <Grid><b>Committer:</b> {result.committer}</Grid>
-                <Grid><b>Message:</b> {result.commitMessage}</Grid>
-                <Grid><b>Date committed:</b> {result.committedDate}</Grid>
-                </Grid> : <></>}
 
-            </div>
-            ))}
-
-            </div>
-        ))}
-        
+                {filteredResultsByDate.map((res, i) => (
+                <Grid key={i} container direction="column" justifyContent="flex-start" alignItems="center" sx={{ m: '5px', backgroundColor: '#9dbbae', borderRadius: "10px", p: "5px" }}>
+                    <Grid><b>Committer:</b> {res.committerName}</Grid>
+                    <Grid><b>Commit message:</b> {res.commitMessage}</Grid>
+                    <Grid><b>Committed date:</b> {res.dateOfCommit}</Grid>
+                </Grid>
+                ))}
             </div>
     );
 }
