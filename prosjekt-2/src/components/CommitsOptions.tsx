3a694cfaf44a5fb4b3158079eed7ab2962e935ed
Karen Villmones <karenvi@stud.ntnu.no>
2022-09-29 12:35:24 +0200

    #28 - Filters commits on names

--- prosjekt-2/src/components/CommitsOptions.tsx
+++ prosjekt-2/src/components/CommitsOptions.tsx
@@ -3,56 +3,81 @@ import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
 import Select, { SelectChangeEvent } from '@mui/material/Select';
 import { useState } from 'react';
-import { Grid } from '@mui/material';
+import { Checkbox, Grid, ListItemText, OutlinedInput } from '@mui/material';
 
 interface Props {
     cleanedResults: Array<any>;
 }
 
-function IssuesOptions({ cleanedResults }: Props) {
+const ITEM_HEIGHT = 48;
+const ITEM_PADDING_TOP = 8;
+const MenuProps = {
+  PaperProps: {
+    style: {
+      maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+      width: 250,
+    },
+  },
+};
 
-    const [name, setName] = useState('');
 
-    const handleChange = (event: SelectChangeEvent) => {
-        setName(event.target.value as string);
-    };
+function CommitsOptions({ cleanedResults }: Props) {
 
-    let emptyList = new Array<string>();
+    const [personName, setPersonName] = useState<string[]>([]);
+
+    const handleChange = (event: SelectChangeEvent<typeof personName>) => {
+        const {
+          target: { value },
+        } = event;
+        console.log(value);
+        console.log(typeof value);
+        setPersonName(
+          // On autofill we get a stringified value.
+          typeof value === 'string' ? value.split(",") : value,
+        );
+      };
+
+    let names = new Array<string>();
 
     cleanedResults.map((result, i) => {
-        if (!emptyList.includes(result.committer)) {
-            emptyList.push(result.committer);
+        if (!names.includes(result.committer)) {
+            names.push(result.committer);
         }
     });
 
-    //console.log(emptyList);
+    console.log(names);
+    console.log(personName);
 
     return (
         <div>
-            <FormControl fullWidth>
-                <InputLabel id="demo-simple-select-label">Filter on assignee name</InputLabel>
-                <Select
-                    labelId="demo-simple-select-label"
-                    id="demo-simple-select"
-                    value={name}
-                    label="Name"
-                    onChange={handleChange}
-                >
+            {/* Inspiration from https://codesandbox.io/s/urnvxd?file=/demo.tsx:1221-1940 */}
+        <FormControl sx={{ m: 1, width: 300 }}>
+            <InputLabel id="checkbox-dropdown">Select names</InputLabel>
+            <Select
+            labelId="checkbox-dropdown"
+            id="select-multiple-dropdown"
+            multiple
+            value={personName}
+            onChange={handleChange}
+            input={<OutlinedInput label="Select names" />}
+            renderValue={(selected) => selected.join(', ')}
+            MenuProps={MenuProps}
+            >
+            {names.map((name) => (
+                <MenuItem key={name} value={name}>
+                <Checkbox checked={personName.indexOf(name) > -1} />
+                <ListItemText primary={name} />
+                </MenuItem>
+            ))}
+            </Select>
+        </FormControl>
 
-                    {emptyList.map((n, i) => (
-                        <MenuItem value={n} key={i}>
-                            {n}
-                        </MenuItem>
-                    ))}
-                </Select>
-            </FormControl>
-            <div>
-
-        Commits made by {name}
-        {cleanedResults.map((result, i) => (
+        {personName.map((committerName, j) => (
+            <div key={j}>
+            {cleanedResults.map((result, i) => (
             <div key={i}>
                 
-                {result.committer === name  ? 
+                {result.committer === committerName  ? 
                 <Grid container direction="column" justifyContent="flex-start" alignItems="center" sx={{ m: '5px', backgroundColor: '#9dbbae', borderRadius: "10px", p: "5px" }}>
                 <Grid><b>Committer:</b> {result.committer}</Grid>
                 <Grid><b>Message:</b> {result.commitMessage}</Grid>
@@ -60,10 +85,13 @@ function IssuesOptions({ cleanedResults }: Props) {
                 </Grid> : <></>}
 
             </div>
-        ))}
+            ))}
+
             </div>
-        </div>
+        ))}
+        
+            </div>
     );
 }
 
-export default IssuesOptions;
+export default CommitsOptions;
