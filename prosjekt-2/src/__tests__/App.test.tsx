8009781ebc0377df28b37bd1ae760b9eb82ca2e2
marielfd <marielfd@stud.ntnu.no>
2022-09-30 10:48:57 +0200

    Added two tests, none of them work. Fix.

--- prosjekt-2/src/__tests__/App.test.tsx
+++ prosjekt-2/src/__tests__/App.test.tsx
@@ -1,11 +1,7 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import App from '../App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
-
-//Change this test 
+it("renders page correctly", () => {
+  const {container} = render(<App/>); 
+  expect(container).toMatchSnapshot(); 
+}); 
--- prosjekt-2/src/__tests__/SendLink.test.tsx
+++ prosjekt-2/src/__tests__/SendLink.test.tsx
@@ -1,5 +1,7 @@
-import renderer from '@testing-library/react'
 import SendLink from '../components/SendLink'
+import {render, screen} from '@testing-library/react'; 
 
-
-
+test("tests that 'issues' exist in the document", () => {
+    render(<SendLink/>)
+    expect(screen.getByText("Issues")).toBeInTheDocument(); 
+}); 
