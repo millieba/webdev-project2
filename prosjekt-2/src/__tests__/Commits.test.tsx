41b762beea9c4d66e650d9c9dc1963261efcf7fd
marielfd <marielfd@stud.ntnu.no>
2022-09-30 17:38:45 +0200

    Deleted outdated tests

--- prosjekt-2/src/__tests__/Commits.test.tsx
+++
@@ -1,8 +0,0 @@
-import {render, screen} from '@testing-library/react'
-import Commits from '../components/Commits';
-
-
-test("See that commits show up", () => {
-    render(<Commits accessToken={''} projectId={''}/>) 
-    
-});
