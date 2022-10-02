f3f6fc0fe3bfba12997331cdca2c3b548a9be162
marielfd <marielfd@stud.ntnu.no>
2022-10-02 18:07:52 +0200

    Removed unused imports etc.

---
+++ prosjekt-2/src/__tests__/UserInput.test.tsx
@@ -0,0 +1,8 @@
+import { render } from "@testing-library/react";
+import { unmountComponentAtNode } from "react-dom";
+import { act } from "react-dom/test-utils";
+import UserInput from "../components/UserInput"
+
+it("Changes on user input", () => {
+    
+}); 
