3e8ee71ff3ce0f1ff4444d4465c9dd71f4a47508
marielfd <marielfd@stud.ntnu.no>
2022-10-02 15:40:57 +0200

    Removed old comments

--- prosjekt-2/src/__tests__/SendLink.test.tsx
+++
@@ -1,9 +0,0 @@
-import { getByTestId, render, screen } from "@testing-library/react";
-import SendLink from "../components/SendLink";
-
-test("test that submit-button is disabled", () => {
-    render(<SendLink/>); 
-
-    const button = screen.getByTitle("access-token");  
-    expect(button).toHaveAttribute('disabled'); 
-})
