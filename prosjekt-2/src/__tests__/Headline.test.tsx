8b656d49d5bb430445780dc199fc79fad1a9ee9c
Karen Villmones <karenvi@stud.ntnu.no>
2022-10-03 20:47:48 +0200

    Forgot to change the headline of the test #32

--- prosjekt-2/src/__tests__/Headline.test.tsx
+++ prosjekt-2/src/__tests__/Headline.test.tsx
@@ -2,7 +2,7 @@ import { render, screen } from '@testing-library/react'
 import Headline from "../components/Headline"
 import '@testing-library/jest-dom'
 
-test("Changes on user input", () => {
+test("Is the headline visible", () => {
     render(<Headline />)
     const headline = screen.getByTestId('headline')
     expect(headline).toBeInTheDocument();
