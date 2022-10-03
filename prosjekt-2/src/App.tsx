644d850e4b66740d136e032da383c12ebb37af67
marielfd <marielfd@stud.ntnu.no>
2022-10-03 19:28:44 +0200

    Does not work :)

--- prosjekt-2/src/App.tsx
+++ prosjekt-2/src/App.tsx
@@ -1,4 +1,4 @@
-import { ThemeContext } from "./contexts/ThemeContext"
+import { ThemeContext } from "./contexts/ThemeContext";
 import { useContext, useState } from "react";
 import "./App.css";
 import UserInput from "./components/UserInput";
@@ -6,20 +6,17 @@ import DarkMode from "./components/DarkMode";
 import { GlobalStyles } from "@mui/material";
 
 function App() {
-  const {themes} = useContext(ThemeContext); 
-  const [isDarkMode] = useState(false); 
+  const { themes } = useContext(ThemeContext);
+  const [isDarkMode] = useState(false);
+  
   const theme = isDarkMode ? themes.dark : themes.light;
 
-//   const toggleDarkMode = () => {
-//     localStorage.setItem("isDarkMode", JSON.stringify(!isDarkMode));
-//     setIsDarkMode(!isDarkMode); 
-// };  
-
   return (
-    <div className="App"
-    // onChange={toggleDarkMode}
-    style={{color: theme.textcolor, backgroundColor: theme.backgroundColor }}>
-      <GlobalStyles styles={{ body: { backgroundColor: theme.backgroundColor } }} />
+    <div
+      className="App"
+      style={{ color: theme.textcolor, backgroundColor: theme.backgroundColor }}>
+      <GlobalStyles
+        styles={{ body: { backgroundColor: theme.backgroundColor } }}/>
       <DarkMode />
       <UserInput />
     </div>
--- prosjekt-2/src/components/DarkMode.tsx
+++ prosjekt-2/src/components/DarkMode.tsx
@@ -3,19 +3,24 @@ import { useContext, useState } from "react";
 import { IconButton } from "@mui/material";
 import { Brightness4, Brightness7 } from "@mui/icons-material";
 
-export default function DarkMode() { 
-  const {themes} = useContext(ThemeContext); 
-  const [isDarkMode, setIsDarkMode] = useState(false); 
+export default function DarkMode() {
+  const { themes } = useContext(ThemeContext);
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
   const theme = isDarkMode ? themes.dark : themes.light;
 
   const toggleDarkMode = () => {
-      localStorage.setItem("isDarkMode", JSON.stringify(!isDarkMode));
-      setIsDarkMode(!isDarkMode); 
+    localStorage.setItem("isDarkMode", JSON.stringify(!isDarkMode));
+    setIsDarkMode(!isDarkMode);
   };
 
   return (
-    <IconButton className="darkButton" onClick={toggleDarkMode} style={{color: theme.darkbutton}}>
+    <IconButton
+      className="darkButton"
+      onClick={toggleDarkMode}
+      style={{ color: theme.darkbutton }}
+    >
       {!isDarkMode ? <Brightness4 /> : <Brightness7 />}
-    </IconButton> 
+    </IconButton>
   );
-} 
+}
